04eee973044f11184a28ac6528089a831e57dd6f
hdoukas <ch.doukas@gmail.com>
2014-07-04 23:15:39 +0200

    updates - servioticy node

---
+++ storage/servioticy/120-servioticy.js
@@ -0,0 +1,199 @@
+/**
+* pusher.js
+* Subscription module for the Pusher service (www.pusher.com)
+* Requires 'pusher' and 'pusher-client' modules.
+*
+* Copyright 2014 Charalampos Doukas, @BuildingIoT
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+module.exports = function(RED) {
+    "use strict";
+
+    var http = require('http');
+
+
+
+     function ServioticyReadNode(n) {
+        // Create a RED node
+        RED.nodes.createNode(this,n);
+
+        var node = this;
+        var credentials = RED.nodes.getCredentials(n.id);
+
+        if ((credentials) && (credentials.hasOwnProperty("auth_token"))) { this.appkey = credentials.auth_token; }
+        else { this.error("No auth token set for input node"); }
+
+        //get parameters from user
+        this.soid = n.soid;
+        this.stream = n.stream;
+        this.channel = n.channel;
+
+        this.on("input", function(msg){
+            var post_options = {
+                host: 'api.servioticy.com',
+                port: '80',
+                path: '/'+this.soid+'/streams/'+this.stream+'/lastUpdate',
+                method: 'GET',
+                headers: {
+                    'authorization': ''+this.appkey
+                }
+            };
+
+
+            var post_req = http.request(post_options, function(res) {
+                res.setEncoding('utf8');
+
+                res.on('data', function (chunk) {
+                    //console.log('Response: ' + chunk);
+                    var channel = this.channel;
+                    var result = JSON.parse(chunk);
+                    //console.log(chunk);
+
+                    //console.log(result["data"][0]["channels"]);
+                    //var name = ''+result["data"][0]["channels"];
+                    //console.log(name);
+                    //if(name.indexOf("temperature") > -1) {
+                        var value = result["data"][0]["channels"][node.channel]['current-value'];
+                        var lastUpdate = result["data"][0]["lastUpdate"];
+                        var msg = {};
+                        msg.payload = value;
+                        msg.lastUpdate = lastUpdate;
+                        node.send(msg);
+                   // }
+                   // else {
+                   //     var msg = {};
+                   //     msg.payload = "no channel with that name found!";
+                   //     node.send(msg);
+                   //}
+
+
+
+
+                });
+            });
+
+            // post the data
+            post_req.write('');
+            post_req.end();
+
+        });
+
+        this.on("close", function() {
+        });
+
+    }
+
+    function ServioticyWriteNode(n) {
+        // Create a RED node
+        RED.nodes.createNode(this,n);
+
+        var node = this;
+        var credentials = RED.nodes.getCredentials(n.id);
+
+        if ((credentials) && (credentials.hasOwnProperty("auth_token"))) { this.appkey = credentials.auth_token; }
+        else { this.error("No auth token set for input node"); }
+
+        //get parameters from user
+        this.soid = n.soid;
+        this.stream = n.stream;
+        this.channel = n.channel;
+
+        this.on("input", function(msg){
+            var sensor_value = msg.payload;
+            var post_data = "{\"channels\": {\""+this.channel+"\": {\"current-value\": \""+sensor_value+"\"}},\"lastUpdate\": "+new Date().getTime()+"}";
+
+            var post_options = {
+                host: 'api.servioticy.com',
+                port: '80',
+                path: '/'+this.soid+'/streams/'+this.stream,
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'authorization': ''+this.appkey,
+                    'Content-Length': post_data.length
+                }
+            };
+
+
+            var post_req = http.request(post_options, function(res) {
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    //console.log('Response: ' + chunk);
+                    var msg = {};
+                    msg.payload = chunk;
+                    node.send(msg);
+                });
+            });
+
+            // post the data
+            post_req.write(post_data);
+            post_req.end();
+
+
+        });
+
+        this.on("close", function() {
+        });
+    }
+
+
+     //debugging on the output:
+    var displayResult = function(result) {
+        node.log(result);
+    };
+
+    var displayError = function(err) {
+        node.log("Error: "+err);
+    };
+
+    RED.nodes.registerType("servioticy in",ServioticyReadNode);
+    RED.nodes.registerType("servioticy out",ServioticyWriteNode);
+
+    var querystring = require('querystring');
+
+    RED.httpAdmin.get('/servioticy/:id',function(req,res) {
+        var credentials = RED.nodes.getCredentials(req.params.id);
+        if (credentials) {
+            res.send(JSON.stringify({auth_token:credentials.auth_token}));
+        } else {
+            res.send(JSON.stringify({}));
+        }
+    });
+
+    RED.httpAdmin.delete('/servioticy/:id',function(req,res) {
+        RED.nodes.deleteCredentials(req.params.id);
+        res.send(200);
+    });
+
+    RED.httpAdmin.post('/servioticy/:id',function(req,res) {
+        var body = "";
+        req.on('data', function(chunk) {
+            body+=chunk;
+        });
+        req.on('end', function(){
+            var newCreds = querystring.parse(body);
+            var credentials = RED.nodes.getCredentials(req.params.id)||{};
+
+            if (newCreds.auth_token === null || newCreds.auth_token === "") {
+                delete credentials.auth_token;
+            } else {
+                credentials.auth_token = newCreds.auth_token;
+            }
+
+
+            RED.nodes.addCredentials(req.params.id,credentials);
+            res.send(200);
+        });
+    });
+}
