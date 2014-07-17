da954f9ada9267e9c536706bf3179f2c1bf28ffe
Matthias Dieter Wallnöfer <mdw@samba.org>
2014-07-17 14:36:33 +0200

    servioticy plugin: properly parse and escape data values using JSON API
    Eg. strings need to be escaped with "", numbers don't need these.

--- storage/servioticy/120-servioticy.js
+++ storage/servioticy/120-servioticy.js
@@ -111,7 +111,14 @@ module.exports = function(RED) {
 
         this.on("input", function(msg){
             var sensor_value = msg.payload;
-            var post_data = "{\"channels\": {\""+this.channel+"\": {\"current-value\": \""+sensor_value+"\"}},\"lastUpdate\": "+ Math.round(new Date().getTime() / 1000) +"}";
+            var post_data = {
+                'channels': {},
+                'lastUpdate': Math.round(new Date().getTime() / 1000)
+            };
+            post_data['channels'][this.channel] = {};
+            post_data['channels'][this.channel]['current-value'] = sensor_value;
+            post_data = JSON.stringify(post_data);
+            // console.log(post_data);
 
             var post_options = {
                 host: 'api.servioticy.com',
